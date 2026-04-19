a7bbb0f12af14557ff79d601c4db8fe4e8d91107
Shravan-Chourasiya <shravan.r.chaursiya@gmail.com>
2026-04-19 18:29:11 +0530

    Added Auth Routes and Controllers for Register,Login,LogOut,Delete and Recover Account,Token Rotator,getAccount,Otp Verification

---
+++ src/MainAuth/controllers/auth.controller.ts
@@ -0,0 +1,468 @@
+import { userModel } from "../models/user.model.js";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { sessionModel } from "../models/session.model.js";
+import { config } from "../configs/configs.js";
+import { generateOTP, getOtpHTML } from "../utils/email.utils.js";
+import { sendVerificationEmail } from "../services/email.service.js";
+import { OtpModel as otpModel } from "../models/otp.model.js";
+import type { NextFunction, Request, Response } from "express";
+import {
+	AccountRecoveryHandler,
+	EmailUpdationHandler,
+	EmailVerificationHandler,
+	ResetPasswordHandler,
+} from "../utils/authController.utils.js";
+import * as z from "zod";
+import type { usernameSchema } from "../libs/auth.ZodSchema.js";
+
+export async function registerController(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	const { username, email, password } = req.body;
+	try {
+		const existingUser = await userModel.findOne({ email });
+		if (existingUser) {
+			return res
+				.status(409)
+				.json({ message: "Email Already In Use.Go to Login" });
+		}
+
+		const hashedPass = await bcrypt.hash(password, 12);
+		const user = await userModel.create({
+			username,
+			email,
+			password: hashedPass,
+		});
+
+		if (!user) {
+			return res.status(503).json({
+				message: "User Not Registered.Please try again Later!",
+			});
+		}
+		const otp = generateOTP();
+		const html = getOtpHTML(otp, "verifyEmailOR");
+
+		await sendVerificationEmail(
+			config.GMAIL_USER_EMAIL as string,
+			email,
+			"Email Verification on TaskAPI",
+			html,
+		);
+
+		// const otpHash = crypto
+		// 	.pbkdf2Sync(otp, config.OTP_SALT as string, 10000, 64, `sha512`)
+		// 	.toString(`hex`);
+
+		const otpHash = await bcrypt.hash(otp, 12);
+		const otpObject = await otpModel.create({
+			userId: user._id,
+			otp: otpHash,
+			email: user.email,
+			purpose: "verifyEmailOR",
+		});
+
+		return res.status(201).json({
+			message: "user created successfully!",
+			data: {
+				username: user.username,
+				email: user.email,
+			},
+		});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function loginController(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const { usernameORemail, password } = req.body;
+		const isUser = await userModel.findOne({
+			$or: [{ username: usernameORemail }, { email: usernameORemail }],
+		});
+		if (!isUser) {
+			return res.status(404).json({ message: "User Not Found!" });
+		}
+		const isPasswordCorrect = await bcrypt.compare(password, isUser.password);
+		if (!isPasswordCorrect) {
+			return res.status(403).json({ message: "Invalid Password.Try Again!" });
+		}
+		if (!isUser.isVerified) {
+			return res
+				.status(403)
+				.json({ message: "User Not verified.Verify Your Email!" });
+		}
+		if (isUser.isDeleted) {
+			return res.status(403).json({
+				message:
+					"Account Scheduled for Deletion! Complete Account Recovery Procedure within 30 days to recover your account!",
+			});
+		}
+		const refreshToken = jwt.sign({ id: isUser._id }, config.JWT_SECRET, {
+			expiresIn: "7d",
+		});
+		const accessToken = jwt.sign(
+			{ id: isUser._id, email: isUser.email },
+			config.JWT_SECRET_2,
+			{ expiresIn: 600 },
+		);
+		const rfTokenHash = await bcrypt.hash(refreshToken, 12);
+		const existingSession = await sessionModel
+			.find({ userId: isUser._id, isRevoked: false })
+			.sort({ createdAt: 1 });
+		if (existingSession.length >= 5) {
+			const oldestSession = existingSession[0];
+			await sessionModel.deleteOne({ _id: oldestSession?._id });
+			return res.status(403).json({
+				message:
+					"Maximum Session Limit Reached! Logged Out from Oldest Session!",
+			});
+		}
+		const session = await sessionModel.create({
+			userId: isUser._id,
+			userIP: req.ip || "unknown",
+			userAgents: req.headers["user-agent"] || "unknown",
+			refreshToken: rfTokenHash,
+		});
+
+		res.cookie("rfToken", refreshToken, config.COOKIE_CONF_RT);
+		res.cookie("acToken", accessToken, config.COOKIE_CONF_AT);
+		return res.status(200).json({
+			message: "User Logged in successfully!",
+			data: {
+				username: isUser.username,
+				email: isUser.email,
+			},
+		});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function tokenRotationController(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const rfToken = req.cookies.rfToken;
+		if (!rfToken) {
+			return res.status(400).json({
+				message: "Refresh Token Not Found !",
+			});
+		}
+		const rfTokenDecoded = jwt.verify(rfToken, config.JWT_SECRET) as JwtPayload;
+		const session = await sessionModel.findOne({
+			userId: rfTokenDecoded.id,
+			isRevoked: false,
+		});
+		if (!session) {
+			return res.status(400).json({
+				message: "Refresh token is invalid or revoked",
+			});
+		}
+		const isRfTokenCorrect = await bcrypt.compare(
+			rfToken,
+			session.refreshToken,
+		);
+		if (!isRfTokenCorrect) {
+			return res.status(400).json({
+				message: "Refresh token is invalid ! Login to get a new one.",
+			});
+		}
+		const user = await userModel.findOne({ _id: rfTokenDecoded.id });
+
+		if (!user) {
+			return res.status(422).json({
+				message: "Error Occured in User Validation !",
+			});
+		}
+
+		const acToken = jwt.sign(
+			{ id: user._id, email: user.email },
+			config.JWT_SECRET_2,
+			{ expiresIn: 600 },
+		);
+		const newRfToken = jwt.sign({ id: user._id }, config.JWT_SECRET, {
+			expiresIn: "7d",
+		});
+		const newRfTokenHash = await bcrypt.hash(newRfToken, 12);
+		session.refreshToken = newRfTokenHash;
+		session.isRevoked = false;
+		await session.save();
+
+		res.cookie("rfToken", newRfToken, config.COOKIE_CONF_RT);
+		res.cookie("acToken", acToken, config.COOKIE_CONF_AT);
+		res.status(200).json({
+			message: "AccessToken Refreshed Successfully !",
+		});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function logoutController(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const rfToken = req.cookies.rfToken;
+		if (!rfToken) {
+			return res.status(400).json({
+				message: "Refresh Token Not Found !",
+			});
+		}
+		const rfTokenDecoded = jwt.verify(rfToken, config.JWT_SECRET) as JwtPayload;
+		const session = await sessionModel.findOne({
+			userId: rfTokenDecoded.id,
+			isRevoked: false,
+		});
+		if (!session) {
+			return res.status(400).json({
+				message: "Refresh token is invalid or revoked",
+			});
+		}
+		const isRfTokenCorrect = await bcrypt.compare(
+			rfToken,
+			session.refreshToken,
+		);
+		if (!isRfTokenCorrect) {
+			return res.status(400).json({
+				message: "Refresh token is invalid or revoked",
+			});
+		}
+		session.isRevoked = true;
+		await session.save();
+		res.clearCookie("rfToken");
+		res.clearCookie("acToken");
+		res.status(200).json({
+			message: "User Logged Out Successfully !",
+		});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function updateDetailsController(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const { fieldToUpdate, newValue, password } = req.body;
+		const decodedToken = jwt.verify(
+			req.cookies.acToken,
+			config.JWT_SECRET_2,
+		) as JwtPayload;
+		const user = await userModel.findOne({ email: decodedToken.email });
+		if (!user) {
+			return res.status(404).json({ message: "User Not Found!" });
+		}
+		const isPasswordCorrect = await bcrypt.compare(password, user.password);
+		if (!isPasswordCorrect) {
+			return res.status(403).json({ message: "Invalid Password.Try Again!" });
+		}
+		switch (fieldToUpdate) {
+			case "username":
+				user.username = newValue as z.infer<typeof usernameSchema>;
+				break;
+			case "email":
+				const otp = generateOTP();
+				const html = getOtpHTML(otp, "verifyEmailUP");
+				await sendVerificationEmail(
+					config.GMAIL_USER_EMAIL,
+					user.email,
+					"Email Verification on TaskAPI",
+					html,
+				);
+				const otpHash = await bcrypt.hash(otp, 12);
+				const otpObject = await otpModel.create({
+					userId: user._id,
+					otp: otpHash,
+					email: user.email,
+					purpose: "verifyEmailUP",
+					isTemp: true,
+					fieldToUpdateNewValue: newValue,
+				});
+				break;
+			case "password":
+				const otp2 = generateOTP();
+				const html2 = getOtpHTML(otp2, "resetPassword");
+
+				await sendVerificationEmail(
+					config.GMAIL_USER_EMAIL,
+					user.email,
+					"Password Reset Verification on TaskAPI",
+					html2,
+				);
+
+				const otpHash2 = await bcrypt.hash(otp2, 12);
+				const newPasswordHash = await bcrypt.hash(newValue, 12); // hash immediately
+
+				const otpObject2 = await otpModel.create({
+					userId: user._id,
+					otp: otpHash2,
+					email: user.email,
+					purpose: "resetPassword",
+					isTemp: true,
+					fieldToUpdateNewValue: newPasswordHash, // store hashed password
+				});
+				break;
+			default:
+				return res.status(400).json({ message: "Invalid Field to Update!" });
+		}
+		await user.save();
+		return res.status(200).json({
+			message:
+				"Verification OTP sent to your email! Complete OTP verification to update your details!",
+		});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function deleteAccountController(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const { usernameORemail, password } = req.body;
+		const user = await userModel.findOne({
+			$or: [{ username: usernameORemail }, { email: usernameORemail }],
+		});
+		if (!user) {
+			return res.status(404).json({ message: "User Not Found!" });
+		}
+		const isPasswordCorrect = await bcrypt.compare(password, user.password);
+		if (!isPasswordCorrect) {
+			return res.status(403).json({ message: "Invalid Password.Try Again!" });
+		}
+		if (user.isDeleted) {
+			return res
+				.status(400)
+				.json({ message: "Account Already Scheduled to be Deleted!" });
+		}
+		user.isDeleted = true;
+		await user.save();
+		return res.status(200).json({
+			message:
+				"Account Scheduled to be Deleted! If you want to recover it,Complete Account Recovery Procedure within 30 days and recover your account!",
+		});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function recoverDeletedAccountController(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const { usernameORemail, password } = req.body;
+		const user = await userModel.findOne({
+			$or: [{ username: usernameORemail }, { email: usernameORemail }],
+		});
+		if (!user) {
+			return res.status(404).json({ message: "User Not Found!" });
+		}
+		const isPasswordCorrect = await bcrypt.compare(password, user.password);
+		if (!isPasswordCorrect) {
+			return res.status(403).json({ message: "Invalid Password.Try Again!" });
+		}
+		if (!user.isDeleted) {
+			return res
+				.status(400)
+				.json({ message: "Account is not Scheduled to be Deleted!" });
+		}
+		const otp = generateOTP();
+		const html = getOtpHTML(otp, "account_recovery");
+		await sendVerificationEmail(
+			config.GMAIL_USER_EMAIL,
+			user.email,
+			"Account Recovery Verification on TaskAPI",
+			html,
+		);
+		const otpHash = await bcrypt.hash(otp, 12);
+		const otpObject = await otpModel.create({
+			userId: user._id,
+			otp: otpHash,
+			email: user.email,
+			purpose: "account_recovery",
+			isTemp: true,
+		});
+		return res.status(200).json({
+			message:
+				"OTP Sent to your Email! Complete OTP Verification to recover your account!",
+		});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function getUserAccountController(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const accessToken = req.cookies.acToken;
+		const decodedToken = jwt.verify(
+			accessToken,
+			config.JWT_SECRET_2,
+		) as JwtPayload;
+		const user = await userModel.findOne({ _id: decodedToken.id });
+		if (!user) {
+			return res.status(404).json({ message: "User Not Found!" });
+		}
+		if (user.isDeleted) {
+			return res.status(400).json({
+				message:
+					"Account Scheduled for Deletion! Complete Account Recovery Procedure within 30 days to recover your account!",
+			});
+		}
+		return res.status(200).json({
+			message: "User Account Details Fetched Successfully!",
+			data: {
+				username: user.username,
+				email: user.email,
+				isVerified: user.isVerified,
+			},
+		});
+	} catch (error) {
+		next(error);
+	}
+}
+
+export async function verificationController(
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) {
+	try {
+		const { otp } = req.body;
+		if (req.query.purpose === "ve-em-or") {
+			return EmailVerificationHandler(otp)(req, res, next);
+		} else if (req.query.purpose === "ve-em-up") {
+			return EmailUpdationHandler(otp)(req, res, next);
+		} else if (req.query.purpose === "rs-pa") {
+			return ResetPasswordHandler(otp)(req, res, next);
+		} else if (req.query.purpose === "ac-re") {
+			return AccountRecoveryHandler(otp)(req, res, next);
+		} else {
+			return res
+				.status(403)
+				.json({ message: "Invalid Access for Verification API!" });
+		}
+	} catch (error) {
+		next(error);
+	}
+}
---
+++ src/MainAuth/routes/auth.routes.ts
@@ -0,0 +1,71 @@
+import express from "express";
+import * as authControllers from "../controllers/auth.controller.js";
+import {
+	accessTokenHandler,
+	refreshTokenHandler,
+} from "../middlewares/tokenHandler.middleware.js";
+import { otpRateLimiter } from "../middlewares/rateLimiting.middleware.js";
+import { ZodValidatorMiddleware } from "../middlewares/zodValidation.middleware.js";
+import {
+	loginDeleteRecoverAccSchema,
+	otpSchema,
+	registerSchema,
+} from "../libs/auth.ZodSchema.js";
+
+
+const router = express.Router();
+
+router.post(
+	"/register",
+	ZodValidatorMiddleware(registerSchema),
+	authControllers.registerController,
+);
+
+router.post(
+	"/verify",
+	ZodValidatorMiddleware(otpSchema),
+	otpRateLimiter,
+	authControllers.verificationController,
+);
+
+router.post(
+	"/login",
+	ZodValidatorMiddleware(loginDeleteRecoverAccSchema),
+	authControllers.loginController,
+);
+
+router.delete("/logout", accessTokenHandler, authControllers.logoutController);
+
+router.patch(
+	"/account/update",
+	accessTokenHandler,
+	authControllers.updateDetailsController,
+);
+
+router.delete(
+	"/account/delete",
+	ZodValidatorMiddleware(loginDeleteRecoverAccSchema),
+	accessTokenHandler,
+	authControllers.deleteAccountController,
+);
+
+router.patch(
+	"/account/recover",
+	ZodValidatorMiddleware(loginDeleteRecoverAccSchema),
+	refreshTokenHandler,
+	authControllers.recoverDeletedAccountController,
+);
+
+router.get(
+	"/account/info",
+	accessTokenHandler,
+	authControllers.getUserAccountController,
+);
+
+router.post(
+	"/token/refresh",
+	refreshTokenHandler,
+	authControllers.tokenRotationController,
+);
+
+export { router as authRouter };
