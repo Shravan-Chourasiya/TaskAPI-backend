2344e4ddb4117e09787628f8e53d59cd765ee740
Shravan-Chourasiya <shravan.r.chaursiya@gmail.com>
2026-06-09 20:20:50 +0530

    Add rate limiting constants for API key updates and enhance update schemas

--- src/libs/zod/apikey.zodschema.ts
+++ src/libs/zod/apikey.zodschema.ts
@@ -7,3 +7,30 @@ export const apiKeyCreationSchema=z.object({
     scopes:z.array(z.string()),
     allowedIPs:z.array(z.string())
 })
+
+export const updateApiKeySchema=z.object({
+    keyId:z.string(),
+    keyUpdatesDetails:z.object({
+        name:z.string().min(5).max(30).optional(),
+        description:z.string().min(10).max(300).optional(),
+        env:z.enum(["production", "development", "test"]).optional(),
+        scopes:z.array(z.string()).optional(),
+        allowedIPs:z.array(z.string()).optional(),
+        keyStatus:z.enum(["active","inactive","revoked"]).optional()
+    })
+})
+
+export const updateApiNameSchema=z.object({
+    keyId:z.string(),
+    newName:z.string().min(5).max(30)
+})  
+
+export const updateApiScopesSchema=z.object({
+    keyId:z.string(),
+    newScopes:z.array(z.string())
+})
+
+export const updateApiIPWhiteListSchema=z.object({ 
+    keyId:z.string(),
+    newIPs:z.array(z.string())
+})
