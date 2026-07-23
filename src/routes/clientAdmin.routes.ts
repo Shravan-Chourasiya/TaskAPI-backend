1e0649757ca8b7a85ffe2be5063fea5681fd7138
Shravan-Chourasiya <shravan.r.chaursiya@gmail.com>
2026-07-23 20:46:20 +0530

    feat(client-admin): add resolveIP, resolveScopes, clientApiRateLimiter, and strictAuthHandler at router level

--- src/routes/clientAdmin.routes.ts
+++ src/routes/clientAdmin.routes.ts
@@ -1,17 +1,24 @@
 import express from "express";
 import { ClientUserStaticMethods } from "../modules/clientauth/types/userMongo.type.js";
 import { ApiKeyStaticMethods } from "../types/mongoModels/apikeys.type.js";
+import { SessionStaticMethods } from "../types/mongoModels/session.type.js";
 import * as clientAdminControllers from "../modules/clientauth/controllers/clientAdmin.controller.js";
 import { apikeyHandlerFunction } from "../middlewares/apikeyhandler.middleware.js";
+import { strictAuthHandlerFunction } from "../middlewares/tokenhandler.middleware.js";
+import { resolveIP } from "../middlewares/ipResolver.middleware.js";
+import { resolveScopes } from "../middlewares/scopeResolver.middleware.js";
 import { createMiddlewareWrapper } from "../utils/middlewareWrapper.js";
 import { asyncErrorHandler } from "../utils/asynchandler.utils.js";
+import { clientApiRateLimiter } from "../modules/clientauth/utils/rateLimiters.js";
 
 export function createClientAdminRouter({
 	clientUserModel,
 	apiKeyModel,
+	sessionModel,
 }: {
 	clientUserModel: ClientUserStaticMethods;
 	apiKeyModel: ApiKeyStaticMethods;
+	sessionModel: SessionStaticMethods;
 }): express.Router {
 	const router = express.Router();
 
@@ -21,7 +28,25 @@ export function createClientAdminRouter({
 		asyncErrorHandler,
 	);
 
-	router.use(apikeyHandler);
+	const strictAuthHandler = createMiddlewareWrapper(
+		sessionModel,
+		strictAuthHandlerFunction,
+		asyncErrorHandler,
+	);
+
+	const ipResolver = createMiddlewareWrapper(
+		apiKeyModel,
+		resolveIP,
+		asyncErrorHandler,
+	);
+
+	const scopeResolver = createMiddlewareWrapper(
+		apiKeyModel,
+		resolveScopes,
+		asyncErrorHandler,
+	);
+
+	router.use(apikeyHandler, ipResolver, scopeResolver, clientApiRateLimiter, strictAuthHandler);
 
 	router.get("/users", (req, res, next) =>
 		clientAdminControllers.getAllUsersList(req, res, next, clientUserModel),
